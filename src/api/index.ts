a738b937305e0ca48586bfde75376c6edb2c925a
Angel dev <albertobernechea@gmail.com>
2026-05-03 15:10:30 +0000

    rady for merge

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,11 +3,11 @@ import type { Transaction } from "../Models/DataTransactions";
 // import { transactionsAll } from "../Models/dummyData";
 
 export const settings = {
-  id: import.meta?.env?.VITE_GOOGLE_ID || "",
-  url: import.meta?.env?.VITE_SHEET || "",
-  password: import.meta?.env?.VITE_PASSWORD || "1234",
-  email: import.meta?.env?.VITE_EMAIL || "admin@gmail.com",
-  isDev: import.meta?.env?.VITE_IS_DEV || true,
+  id: import.meta?.env?.VITE_GOOGLE_ID ,
+  url: import.meta?.env?.VITE_SHEET,
+  password: import.meta?.env?.VITE_PASSWORD ,
+  email: import.meta?.env?.VITE_EMAIL ,
+  isDev: import.meta?.env?.VITE_IS_DEV ,
 };
 
 export async function getSheetData(sheetName: string) {
